bfc94d40aff182d920557bd0c084d7b5ca77bcc3
Соломон Захаров <solomon7and@gmail.com>
2021-05-17 17:19:10 +0500

    Урок 2 Создать  4 обьекта

--- js/script.js
+++ js/script.js
@@ -1,2 +1,32 @@
-let hi = 'Привет';
-console.log(hi);
+let hello = 'Привет';
+console.log(hello);
+
+let num;
+let score;
+let question;
+let answer;
+let currectAnswer;
+
+let questions1 = {
+  question: question,
+  answer: answer,
+  currectAnswer: currectAnswer
+}
+
+let questions2 = {
+  question: question,
+  answer: answer,
+  currectAnswer: currectAnswer
+}
+
+let questions3 = {
+  question: question,
+  answer: answer,
+  currectAnswer: currectAnswer
+}
+
+let questions4 = {
+  question: question,
+  answer: answer,
+  currectAnswer: currectAnswer
+}
