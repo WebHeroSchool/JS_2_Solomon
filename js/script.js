683bbe83f2c068cc4153458d64e6e9f684d4a5da
Соломон Захаров <solomon7and@gmail.com>
2021-05-18 15:40:21 +0500

    правки

--- js/script.js
+++ js/script.js
@@ -3,24 +3,24 @@ let score;
 
 const questions1 = {
   question: 'Кто создал и когда JS?',
-  answer: 'Брендан Ейх в 1995 году.'
+  answer: 'Брендан Ейх в 1995 году.',
   currectAnswer: '',
 }
 
 const questions2 = {
   question: 'Кто создал и когда JS?',
-  answer: 'Брендан Ейх в 1995 году.'
+  answer: 'Брендан Ейх в 1995 году.',
   currectAnswer: '',
 }
 
 const questions3 = {
   question: 'Кто создал и когда JS?',
-  answer: 'Брендан Ейх в 1995 году.'
+  answer: 'Брендан Ейх в 1995 году.',
   currectAnswer: '',
 }
 
 const questions4 = {
   question: 'Кто создал и когда JS?',
-  answer: 'Брендан Ейх в 1995 году.'
+  answer: 'Брендан Ейх в 1995 году.',
   currectAnswer: '',
 }
